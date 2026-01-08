3a71ce22a3da61f7204100d7df8152a031b2a1fd
Jakub Żytka <jakub.zytka@reef.pl>
2026-01-08 14:55:25 +0100

    cat-images: add minimal subnet demo

--- cat-images/cat_images_subnet/cat_images.py
+++ cat-images/cat_images_subnet/cat_images.py
@@ -1,66 +1,28 @@
-from dataclasses import dataclass
+import time
 
-from core.dsl import flow
-from actors.rest import RestEntryPoint
-from nexus.actors.stringify import Stringify
-from nexus.actors.uppercase_or_error import UppercaseOrError
-from pydantic import BaseModel
+from nexus.core.runtime.actor import Actor
+from nexus.core.runtime.event_bus import EventBus
+from nexus.core.runtime.events import PipeToBus, StopBusEvent
 
+from .validator import make_subnet
 
-@dataclass
-class S3Url(str):
-    """
-    URL of an image stored on S3.
-    """
+if __name__ == "__main__":
+    piping, nodes = make_subnet()
 
+    pipe_to_bus = PipeToBus()
+    actors: list[Actor] = [node.build_actor(pipe_to_bus=pipe_to_bus) for node in nodes]
 
-@dataclass
-class ImageName(str):
-    """
-    Name of an image file.
-    """
+    event_bus = EventBus(piping.pipes, pipe_to_bus, actors)
 
+    for actor in actors:
+        actor.run_loop()
 
-class SingleCatImageInput(BaseModel):
-    """
-    User request model for generating a cat‑augmented image.
+    event_bus.run_loop()
 
-    `image_s3_url` refers to the original
-    background image stored on S3; `image_name` is a file name used in
-    constructing upload keys.
-    """
+    print('Event bus and actors are running. Press Ctrl+C to stop.')
 
-    # this is essentially user data
-    # apart from that we have some additional data kept in the context like
-    # user_id, request_id
-    image_s3_url: S3Url
-    image_name: ImageName
-
-
-class CatImagesValidator:
-    """
-    Definition of the cat‑images subnet validator.  It wires together generic Nexus
-    components with domain‑specific data models and helper functions.
-    """
-
-    entry: RestEntryPoint[SingleCatImageInput] = RestEntryPoint(
-        path="/cat",
-        port=8080,
-        user_data_model=SingleCatImageInput)
-
-    stringify: Stringify[SingleCatImageInput] = Stringify()
-
-    forking_task: UppercaseOrError = UppercaseOrError()
-
-
-    flow: Flow = flow(entry)
-         .then(stringify)
-         .then(forking_task)
-         .then(ok=stringify
-                    .then(stringify),
-               error=stringify)
-
-    # or maybe ?
-    # flow: Flow = Flow.start(entry.output)
-    #                   .then(stringify)
-    #                   .then(entry.input)
+    try:
+        while True:
+            time.sleep(1)
+    except KeyboardInterrupt:
+        pipe_to_bus.put(StopBusEvent())
---
+++ cat-images/cat_images_subnet/validator.py
@@ -0,0 +1,50 @@
+from typing import NewType
+
+from nexus.actors import (
+    RestEntryPoint,
+    Stringify,
+    UppercaseOrError,
+)
+from nexus.core.dsl.piping import Piping
+from pydantic import BaseModel
+
+S3Url = NewType("S3Url", str)
+
+ImageName = NewType("ImageName", str)
+
+class SingleCatImageInput(BaseModel):
+    """
+    User request model for generating a cat‑augmented image.
+
+    `image_s3_url` refers to the original
+    background image stored on S3; `image_name` is a file name used in
+    constructing upload keys.
+    """
+
+    # this is essentially user data
+    # apart from that we have some additional data kept in the context like
+    # user_id, request_id
+    image_s3_url: S3Url
+    image_name: ImageName
+
+
+def make_subnet():
+    entry: RestEntryPoint[SingleCatImageInput] = RestEntryPoint(
+        path="/cat",
+        port=8080,
+        user_data_model=SingleCatImageInput)
+
+    stringify: Stringify[SingleCatImageInput] = Stringify()
+
+    mining_task: UppercaseOrError = UppercaseOrError()
+
+    stringify_error: Stringify[Exception] = Stringify()
+
+    piping = Piping()
+    piping.connect(entry.source, stringify.sink)
+    piping.connect(stringify.ok, mining_task.sink)
+    piping.connect(mining_task.ok, entry.sink)
+    piping.connect(mining_task.error, stringify_error.sink)
+    piping.connect(stringify_error.ok, entry.sink)
+
+    return piping, [entry, stringify, mining_task, stringify_error]
