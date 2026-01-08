73a624af818b9ba55fe9330fd2a26680ff14783c
Jakub Żytka <jakub.zytka@reef.pl>
2026-01-08 14:54:57 +0100

    nexus-template: model DSL nodes with named sinks/sources

--- nexus-template/nexus/core/dsl/nodes.py
+++ nexus-template/nexus/core/dsl/nodes.py
@@ -2,8 +2,10 @@ from __future__ import annotations
 
 import itertools
 import traceback
+from abc import ABC, abstractmethod
 from collections import defaultdict
-from typing import Any, NewType, TypeVar
+from dataclasses import dataclass
+from typing import Any, NewType, TypeVar, override
 
 T = TypeVar("T")
 
@@ -15,6 +17,49 @@ SinkId = NewType("SinkId", str)
 # contraption that owns them
 SourceName = NewType("SourceName", str)
 SinkName = NewType("SinkName", str)
+
+
+@dataclass
+class NodeSources:
+    sources: dict[SourceName, Source[Any]]
+    default_source: Source[Any] | None = None
+
+    def __post_init__(self) -> None:
+        assert self.default_source is None or self.default_source in self.sources.values(), \
+            f"primary_source {self.default_source!r} must be None or a key in sources; " \
+            f"available: {list(self.sources)}"
+        if len(self.sources) == 1:
+            self.default_source = next(iter(self.sources.values()))
+
+    def empty(self) -> bool:
+        return bool(self.sources)
+
+
+@dataclass
+class NodeSinks:
+    sinks: dict[SinkName, Sink[Any]]
+    default_sink: Sink[Any] | None = None
+
+    def __post_init__(self) -> None:
+        assert self.default_sink is None or self.default_sink in self.sinks, \
+            f"primary_sink {self.default_sink!r} must be None or a key in sinks; " \
+            f"available: {list(self.sinks.keys())}"
+        if len(self.sinks) == 1:
+            self.default_sink = next(iter(self.sinks.values()))
+
+
+class Node(ABC):
+    """
+        any element of the processing graph is a Node
+    """
+
+    @abstractmethod
+    def sinks(self) -> NodeSinks:
+        pass
+
+    @abstractmethod
+    def sources(self) -> NodeSources:
+        pass
 
 
 class HasGlobalId:
@@ -32,19 +77,39 @@ class HasGlobalId:
         HasGlobalId.global_ids[self.gid] = traceback.extract_stack()
 
 
-class Source[T](HasGlobalId):
+class Source[T](HasGlobalId, Node):
     """
         A logical endpoint for data production.
     """
 
+    @override
+    def sources(self) -> NodeSources:
+        return NodeSources(sources={
+            SourceName("self"): self
+        })
 
-class Sink[T](HasGlobalId):
+    @override
+    def sinks(self) -> NodeSinks:
+        return NodeSinks(sinks={})
+
+
+class Sink[T](HasGlobalId, Node):
     """
         A logical endpoint for data consumption.
     """
 
+    @override
+    def sources(self) -> NodeSources:
+        return NodeSources(sources={})
 
-class Fork[From, ToLeft, ToRight](HasGlobalId):
+    @override
+    def sinks(self) -> NodeSinks:
+        return NodeSinks({
+            SinkName("self"): self
+        })
+
+
+class Fork[From, ToLeft, ToRight](HasGlobalId, Node):
     """
         A logical data processing unit that forks data from a Sink to two Sources.
     """
@@ -59,8 +124,21 @@ class Fork[From, ToLeft, ToRight](HasGlobalId):
         self.left = Source[ToLeft](gid_prefix=f"{self.gid}-left-source")
         self.right = Source[ToRight](gid_prefix=f"{self.gid}-right-source")
 
+    @override
+    def sinks(self) -> NodeSinks:
+        return NodeSinks(sinks={
+            SinkName("sink"): self.sink,
+        })
 
-class Transform[From, To](Fork[From, To, Exception]):
+    @override
+    def sources(self) -> NodeSources:
+        return NodeSources(sources={
+            SourceName("left"): self.left,
+            SourceName("right"): self.right,
+        })
+
+
+class Transform[From, To](Fork[From, To, Exception], Node):
     # convenient aliases
     ok: Source[To]
     error: Source[Exception]
@@ -76,8 +154,17 @@ class Transform[From, To](Fork[From, To, Exception]):
         self.ok = self.left
         self.error = self.right
 
+    @override
+    def sources(self) -> NodeSources:
+        return NodeSources(
+            sources={
+                SourceName("ok"): self.ok,
+                SourceName("error"): self.error,
+            },
+            default_source=self.ok)
 
-class DoubleTransform[InputFrom, InputTo, OutputFrom, OutputTo](HasGlobalId):
+
+class DoubleTransform[InputFrom, InputTo, OutputFrom, OutputTo](HasGlobalId, Node):
     """
         A logical data processing unit that is a two-way Transform
         - the first Transform converts InputFrom to InputTo
@@ -108,7 +195,25 @@ class DoubleTransform[InputFrom, InputTo, OutputFrom, OutputTo](HasGlobalId):
         self.output_ok = self.output_transform.ok
         self.output_error = self.output_transform.error
 
+    def sources(self) -> NodeSources:
+        return NodeSources(
+            sources={
+                SourceName("input_ok"): self.input_ok,
+                SourceName("input_error"): self.input_error,
+                SourceName("output_ok"): self.output_ok,
+                SourceName("output_error"): self.output_error,
+            }
+        )
+
+    def sinks(self) -> NodeSinks:
+        return NodeSinks(
+            sinks={
+                SinkName("input_sink"): self.input_sink,
+                SinkName("output_sink"): self.output_sink,
+            }
+        )
+
 
 Sources = set[Source[Any]]
 Sinks = set[Sink[Any]]
-Pipes = defaultdict[Source[Any], set[Sink[Any]]]  # we lose type info here for simplicity
+Pipes = defaultdict[Source[Any], set[Sink[Any]]]
