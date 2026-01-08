790b595767a5e94264d3f92b2e31f890209f94ee
Jakub Żytka <jakub.zytka@reef.pl>
2026-01-08 14:55:09 +0100

    nexus-template: add StringPrinter actor

---
+++ nexus-template/nexus/actors/string_printer.py
@@ -0,0 +1,28 @@
+from typing import override
+
+from nexus.context_store import ContextId
+from nexus.core.dsl.nodes import Sink
+from nexus.core.runtime.actor import ActorBuilder
+from nexus.core.runtime.actor_patterns import ConsumerActor
+from nexus.core.runtime.events import PipeToBus
+
+
+class StringPrinter(Sink[str], ActorBuilder):
+    """
+        An actor that prints incoming strings to the console.
+    """
+
+    def __init__(self, gid_prefix: str | None = None) -> None:
+        super().__init__(gid_prefix=gid_prefix)
+
+    @override
+    def build_actor(self, *, pipe_to_bus: PipeToBus) -> StringPrinterActor:
+        return StringPrinterActor(spec=self, pipe_to_bus=pipe_to_bus)
+
+
+class StringPrinterActor(ConsumerActor[str]):
+    def __init__(self, *, spec: StringPrinter, pipe_to_bus: PipeToBus) -> None:
+        super().__init__(spec=spec, pipe_to_bus=pipe_to_bus)
+
+    def _consume(self, ctx: ContextId, payload: str) -> None:
+        print(payload)
