2b27cbbbd15d5b3cdab647458aa2cabad6ca4458
Jakub Żytka <jakub.zytka@reef.pl>
2026-02-11 00:05:07 +0100

    feat: minimal working cat-images subnet

--- cat-images/cat_images/cat_images.py
+++ cat-images/cat_images/cat_images.py
@@ -1,18 +1,23 @@
 import time
 
 from nexus.core.runtime.actor import Actor
+from nexus.core.runtime.context_store import ContextStore, InMemoryContextStorePersistence
 from nexus.core.runtime.event_bus import EventBus
 from nexus.core.runtime.events import PipeToBus, StopBusEvent
 
 from .validator import make_subnet
 
 if __name__ == "__main__":
+    persistence: InMemoryContextStorePersistence = InMemoryContextStorePersistence()
+    context_store: ContextStore = ContextStore.recover_from(persistence).context_store
+
     piping, nodes = make_subnet()
-
     pipe_to_bus = PipeToBus()
-    actors: list[Actor] = [node.build_actor(pipe_to_bus=pipe_to_bus) for node in nodes]
+    actors: list[Actor] = [node.build_actor(pipe_to_bus=pipe_to_bus, context_store=context_store)
+                           for node in nodes]
 
-    event_bus = EventBus(piping.pipes, pipe_to_bus, actors)
+
+    event_bus = EventBus(piping.pipes, pipe_to_bus, actors, context_store)
 
     for actor in actors:
         actor.run_loop()
